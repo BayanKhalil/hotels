c65d14cee6e2d0359c99b3adea179b18837b6f80
samer <s.allaham@ltuc.com>
2021-01-28 12:36:54 +0200

    finished demo for lab5

--- app.js
+++ app.js
@@ -1,90 +1,92 @@
+/*
+Logical operators:
+&& AND
+condition 1 is true and condition 2 is also true 
 
-
-// console.log('hello my age is 32 ');
-
-// console.log(2+3);
-
-// var userName = 'ahmad';
-// // console.log(userName);
-
-// var x = 5;
-
-// console.log(x+=3);
-// x =x+3
-// console.log('hello ' + 'samer');
-// concatination 
-
-// console.log(3 >= 3);
-// console.log('samer' == 'samerallaham')
-
-
-/* 
-Datatypes:
-- string
-- numbers
-- boolean:
-it can only be true or false
-
-
-
-Operaters:
-sympol that does action or something
-
-arithmatic operators:
-+ plus
-- minus
-* multiply
-/ devide
-% reminder of 
-** power of
-
-asignemnt operators:
-= 
-+= 
--=
-
-ex:
-var x = 10;
-x+=5 is the same as 
-x=x+5
-
-comparison operators:
-true or false
-
-== compare the value only
-=== compare value and datatype
-!= not equal to value alone
-!==  not equal to value alone and datatype
-< bigger than or smaller than
->= bigger than or equal
+|| OR
+condition 1 is true OR condition 2 is true
 
 */
 
 
+// example:
+
+// var age = prompt('what is your age');
+
+// console.log(age);
+
+// if (age >= 18 && age<=35){
+//     document.write('welcome ');
+// }else if(age < 18 || age>35){
+//     document.write('sorry you can\'t enter');
+// }else{
+//     document.write('not allowed');
+// }
 
 
-// alert('hello ahmad');
+// ****** DEMO ******
 
 
 
-var message='';
+/*
 
-var hourNow = prompt('what time is it ?');
-console.log(hourNow);
+To do list:
 
-if (hourNow > 18){
-    message = 'Good evening';
+-add image based on user input ask the user what size he wants
 
-}else if ( hourNow > 12){
-    message = 'good afternoon';
-}else if (hourNow>=0){
-    message = 'good morning';
-}else{
-    message = 'welcome'
+-keep asking the user for a valid input he can only answer with big or small
+
+-Ask how many pictures does he/she wants and show them on the page
+
+*/
+
+var order = prompt('what size do you want big or small?');
+
+while(order !=='big' && order!=='small'){
+    order =prompt('please write only big or small ')
 }
 
-document.write('<h3>' + message +'</h3>');
 
 
-var userName = prompt('whats you name?');
-alert('hello ' +userName);
+var room = '';
+
+if(order === 'big'){
+    room ='<img src="images/big.jpg" />';
+}else if(order === 'small'){
+    room = '<img src="images/small.jpg" />';
+}
+
+var result = '';
+
+// result = room;
+
+var numRooms=prompt('how many rooms do you want');
+
+for(var i = 0 ; i<numRooms ; i++){
+    console.log(i);
+    // document.write(result);
+    result =result+  room;
+}
+
+
+document.write(result);
+
+
+
+
+// for(var i = 0 ; i<= 10 ; i=i+2){
+//     // i = 0
+
+//     document.write('<br>'+'hello ');
+// }
+
+// var i = 0;
+// while(i < 5){
+
+//     document.write('hello')
+//      i = i +1;
+//     //  i++
+// }
+
+
+
---
+++ lab4.js
@@ -0,0 +1,109 @@
+
+
+// console.log('hello my age is 32 ');
+
+// console.log(2+3);
+
+// var userName = 'ahmad';
+// // console.log(userName);
+
+// var x = 5;
+
+// console.log(x+=3);
+// x =x+3
+// console.log('hello ' + 'samer');
+// concatination 
+
+// console.log(3 >= 3);
+// console.log('samer' == 'samerallaham')
+
+
+/* 
+Datatypes:
+- string
+- numbers
+- boolean:
+it can only be true or false
+
+
+
+Operaters:
+sympol that does action or something
+
+arithmatic operators:
++ plus
+- minus
+* multiply
+/ devide
+% reminder of 
+** power of
+
+asignemnt operators:
+= 
++= 
+-=
+
+ex:
+var x = 10;
+x+=5 is the same as 
+x=x+5
+
+comparison operators:
+true or false
+
+== compare the value only
+=== compare value and datatype
+!= not equal to value alone
+!==  not equal to value alone and datatype
+< bigger than or smaller than
+>= bigger than or equal
+
+*/
+
+// alert('hello ahmad');
+
+
+
+
+
+
+// var message='';
+
+// var hourNow = prompt('what time is it ?');
+// console.log(hourNow);
+
+// if (hourNow > 18){
+//     message = 'Good evening';
+
+// }else if ( hourNow > 12){
+//     message = 'good afternoon';
+// }else if (hourNow>=0){
+//     message = 'good morning';
+// }else{
+//     message = 'welcome'
+// }
+
+// document.write('<h3>' + message +'</h3>');
+
+
+// var userName = prompt('whats you name?');
+// alert('hello ' +userName);
+
+
+/* multible lines of comments */
+
+// when does semi colon shows error?
+// why if string is empty it considers it as 0
+
+// review
+// var age = prompt('what is your age');
+
+// console.log(age);
+
+// if (age >= 18){
+//     document.write('welcome ');
+// }else if(age < 18){
+//     document.write('sorry this is only for adults');
+// }else{
+//     document.write('please add a number');
+// }
