d0beb4bb378dfc0ea02b28b3f70a27cb97ae4136
samer <s.allaham@ltuc.com>
2021-01-31 12:14:52 +0200

    finished CSS and functions demo

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ condition 1 is true OR condition 2 is true
 // ****** DEMO ******
 
 
-
 /*
 
 To do list:
@@ -39,39 +38,6 @@ To do list:
 -Ask how many pictures does he/she wants and show them on the page
 
 */
-
-var order = prompt('what size do you want big or small?');
-
-while(order !=='big' && order!=='small'){
-    order =prompt('please write only big or small ')
-}
-
-
-
-var room = '';
-
-if(order === 'big'){
-    room ='<img src="images/big.jpg" />';
-}else if(order === 'small'){
-    room = '<img src="images/small.jpg" />';
-}
-
-var result = '';
-
-// result = room;
-
-var numRooms=prompt('how many rooms do you want');
-
-for(var i = 0 ; i<numRooms ; i++){
-    console.log(i);
-    // document.write(result);
-    result =result+  room;
-}
-
-
-document.write(result);
-
-
 
 
 // for(var i = 0 ; i<= 10 ; i=i+2){
@@ -87,6 +53,95 @@ document.write(result);
 //      i = i +1;
 //     //  i++
 // }
+
+
+
+// function declaration
+
+function welcomeMessage() {
+    // for prinitng message
+
+    var message = '';
+    // input
+    var hourNow = prompt('what time is it ?');
+    console.log(hourNow);
+
+
+    // code
+    if (hourNow > 18) {
+        message = 'Good evening';
+
+    } else if (hourNow > 12) {
+        message = 'good afternoon';
+    } else if (hourNow >= 0) {
+        message = 'good morning';
+    } else {
+        message = 'welcome'
+    }
+
+    // output
+    return document.write('<h3>' + message + '</h3>');
+
+}
+
+
+// console.log(message);
+
+
+
+
+
+
+// function expression:
+var showImage = function() {
+    var order = prompt('what size do you want big or small?');
+    
+    while (order !== 'big' && order !== 'small') {
+        order = prompt('please write only big or small ')
+    }
+    
+    var room = '';
+    
+    if (order === 'big') {
+        room = '<img src="images/big.jpg" />';
+    } else if (order === 'small') {
+        room = '<img src="images/small.jpg" />';
+    }
+    
+    var result = '';
+    
+    // result = room;
+    
+    var numRooms = prompt('how many rooms do you want');
+    
+    for (var i = 0; i < numRooms; i++) {
+        console.log(i);
+        // document.write(result);
+        result = result + room;
+    }
+    
+    
+    return document.write(result);
+
+
+}
+
+
+// for image
+
+
+
+
+// function sum(){
+//     var num1 = 50;
+//     var num2 = 10;
+//     var sumNumbers = num1 + num2;
+
+//     return sumNumbers
+// }
+
+// console.log( sum() );
+
 
 
 
